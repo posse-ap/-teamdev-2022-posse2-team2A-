e0c3bbde0d423c7b83a2c59c3e5755b9181a76cc
張致豪 <chohtsuyoshi@chouitasugounoMacBook-Air.local>
2022-05-23 23:27:25 +0900

    表を完成した

--- src/admin/index.php
+++ src/admin/index.php
@@ -45,66 +45,124 @@ $customers = $stmt->fetchAll(PDO::FETCH_ASSOC);
             </li>
         <?php endforeach; ?>
     </ul>
-    <a href="logout.php">ログアウト</a>
+    <!-- <a href="logout.php">ログアウト</a> -->
     <div class="tab-panel">
-	<!--タブ-->
-	<ul class="tab-group">
-		<li class="tab tab-A is-active">企業一覧</li>
-		<li class="tab tab-B">企業情報追加</li>
-		<li class="tab tab-C">企業情報変更</li>
-	</ul>
-	
-	<!--タブを切り替えて表示するコンテンツ-->
-    <div class="panel-group">
-		<div class="panel tab-A is-show">
-            <div class="w-full mt-3 scroll">
-            <table>
-        <tr>
-            <th>企業名</th>
-            <th>表示ステータス</th>
-            <th>担当者名</th>
-            <th>部署名</th>
-            <th>役所名</th>
-            <th>メールアドレス</th>
-            <th>電話番号</th>
-            <th>今月の問い合わせ人数</th>
-            <th>今月の報酬予定額</th>
-        </tr>
-        <tr class="white">
-            <td>amazon</td>
-            <td>表示</td>
-            <td>takaharatomoaki
-            </td>
-            <td>a</td>
-            <td>a</td>
-            <td>a</td>
-            <td>a</td>
-            <td>a</td>
-            <td>a</td>
-        </tr>
-        <tr class="mint">
-            <td>amazon</td>
-            <td>表示</td>
-            <td>たか</td>
-            <td>aaaaaaaaaaaaaaaaaaaaaaaa</td>
-            <td>aaaaaaaaaaaaaa</td>
-            <td>aaaaaaaaaaaaaaaaaaa</td>
-            <td>a</td>
-            <td>a</td>
-            <td>a</td>
-        </tr>
-    </table>
+        <!--タブ-->
+        <ul class="tab-group">
+            <li class="tab tab-A is-active">企業一覧</li>
+            <li class="tab tab-B">企業情報追加</li>
+            <li class="tab tab-C">企業情報変更</li>
+        </ul>
+
+        <!--タブを切り替えて表示するコンテンツ-->
+        <div class="panel-group">
+            <div class="panel tab-A is-show" id="tabApanel">
+                <div class="w-full mt-3 scroll">
+                    <table>
+                        <tr>
+                            <th>企業名</th>
+                            <th>表示ステータス</th>
+                            <th>担当者名</th>
+                            <th>部署名</th>
+                            <th>役所名</th>
+                            <th>メールアドレス</th>
+                            <th>電話番号</th>
+                            <th>今月の問い合わせ人数</th>
+                            <th>今月の報酬予定額</th>
+                        </tr>
+                        <tr class="white">
+                            <td onclick="showInfo({hidden:panel, show:memberDetail})">amazon</td>
+                            <td>表示</td>
+                            <td>takaharatomoaki
+                            </td>
+                            <td>a</td>
+                            <td>a</td>
+                            <td>a</td>
+                            <td>a</td>
+                            <td>a</td>
+                            <td>a</td>
+                        </tr>
+                        <tr class="mint">
+                            <td>amazon</td>
+                            <td>表示</td>
+                            <td>たか</td>
+                            <td>aaaaaaaaaaaaaaaaaaaaaaaa</td>
+                            <td>aaaaaaaaaaaaaa</td>
+                            <td>aaaaaaaaaaaaaaaaaaa</td>
+                            <td>a</td>
+                            <td>a</td>
+                            <td>a</td>
+                        </tr>
+                    </table>
+                </div>
             </div>
+            <div id="memberDetail" class="memberDetail">
+                <div class="status">
+                    <div class="w-full text-center">＜　４月　＞</div>
+                    <div class="w-full flex justify-center">
+                        <div class="flex items-center m-6">エントリー人数</div>
+                        <div class="text-center m-6"><span class="text-3xl font-bold">10</span><br>人</div>
+                        <div class="flex items-center m-6">報酬予定額</div>
+                        <div class="text-center m-6"><span class="text-2xl font-bold">200000</span><br>円</div>
+                    </div>
+                </div>
+                <div class="w-full mt-3 scroll">
+                    <table>
+                        <tr>
+                            <th>対応ステータス</th>
+                            <th>氏名</th>
+                            <th>氏名カナ</th>
+                            <th>生年月日</th>
+                            <th>性別</th>
+                            <th>メールアドレス</th>
+                            <th>電話番号</th>
+                            <th>住所</th>
+                            <th>学校学部学科</th>
+                            <th>卒業年度</th>
+                            <th>質問等</th>
+                            <th>非対応理由</th>
+                        </tr>
+                        <tr class="white">
+                            <td>amazon</td>
+                            <td>表示</td>
+                            <td>takaharatomoaki</td>
+                            <td>a</td>
+                            <td>a</td>
+                            <td>a</td>
+                            <td>a</td>
+                            <td>a</td>
+                            <td>a</td>
+                            <td>a</td>
+                            <td>a</td>
+                            <td>a</td>
+                        </tr>
+                        <tr class="mint">
+                            <td>amazon</td>
+                            <td>表示</td>
+                            <td>たか</td>
+                            <td>aaaaaaaaaaaaaaaaaaaaaaaa</td>
+                            <td>aaaaaaaaaaaaaa</td>
+                            <td>aaaaaaaaaaaaaaaaaaa</td>
+                            <td>a</td>
+                            <td>a</td>
+                            <td>a</td>
+                            <td>a</td>
+                            <td>a</td>
+                            <td>a</td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="w-full text-green-600 text-sm text-right ml-auto" onclick="returninfo({undo:memberDetail, display:panel})">企業一覧に戻る</div>
+            </div>
+            <div id="panel" class="panel tab-B">Content-B</div>
+            <div class="panel tab-C">Content-C</div>
+        </div>
     </div>
-		<div class="panel tab-B">Content-B</div>
-		<div class="panel tab-C">Content-C</div>
-	</div>
-</div>
-<footer>
-    <!-- jquery -->
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-    <script src="../user/script.js"></script>
-</footer>
+    <footer>
+        <!-- jquery -->
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+        <script src="../user/script.js"></script>
+    </footer>
 </body>
 
 </html>
