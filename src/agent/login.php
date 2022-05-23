17461eabd551686f3b0e7da81e4945b574eec337
takahara tomoaki <jihyun8286@gmail.com>
2022-05-23 20:49:51 +0900

    #11 企業ログイン完成(ほぼ管理者ログインのコピペ)

--- src/agent/login.php
+++ src/agent/login.php
@@ -26,21 +26,33 @@ if (!empty($_POST)) {
 <html lang="ja">
 
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../user/style.css">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     <title>企業ログイン</title>
 </head>
 
 <body>
-    <div>
-        <h1>企業ログイン</h1>
-        <form action="/agent/login.php" method="POST">
-            <p>メールアドレス：<input type="email" name="email" required></p>
-            <p>パスワード：<input type="password" required name="password"></p>
-            <input type="submit" value="ログイン">
-        </form>
+<header class="boozer-login-icon">
+    <div class="icon">
+        <img src="../user/_header phpに入れてる.png" alt="">
     </div>
+    </header>
+        <div class="text-center">
+            <h1 class="text-2xl font-bold mt-4 mb-4 sm:text-4xl">企業ログイン</h1>
+        </div>
+        <form class="width-full flex p-4 justify-center" action="/agent/login.php" method="POST">
+            <div>
+                <input class="block border-2 my-3 rounded mb-7" type="email" name="email" size="20" placeholder="メールアドレス" required>
+                <input class="block border-2 my-3 rounded mb-7" type="password" size="20" placeholder="パスワード" required name="password">
+                <input class="block my-0 mx-auto mt-4 py-3 rounded" type="submit" value="ログイン">
+            </div>
+        </form>
+        <div class="text-center">
+            <a href="" class="text-xs">パスワードを忘れた方へ</a>
+        </div>
 </body>
 
 </html>
