dda800afb5c4ae300bbbd2abb6b5fb6800421916
Eiki Watanabe <basketball.slamdunk1@icloud.com>
2022-05-25 16:08:03 +0900

    inputタグ削除

--- src/user/check.php
+++ src/user/check.php
@@ -114,79 +114,79 @@ if ($_SESSION['cart'] == NULL) {
                 <tr>
                     <th class="contact-item">姓名<span style="color:red">*</span><br>Name(Kanji)</th>
                     <td class="contact-body">
-                        <span class="inline-block">姓：<input class="m-2" type="text" name="first_name" value="<?= $_POST['first_name']; ?>" readonly></span>
-                        <span class="inline-block">名：<input class="m-2" type="text" name="last_name" value="<?= $_POST['last_name']; ?>" readonly></span>
+                        <span class="inline-block">姓：<?= $_POST['first_name']; ?> </span>
+                        <span class="inline-block">名：<?= $_POST['last_name']; ?></span>
                     </td>
                 </tr>
                 <tr>
                     <th class="contact-item">フリガナ<span style="color:red">*</span><br>Name(Kana)</th>
                     <td class="contact-body">
-                        <span class="inline-block">セイ：<input class="m-2" type="text" name="first_name_kana" value="<?= $_POST['first_name_kana']; ?>" readonly></span>
-                        <span class="inline-block">カナ：<input class="m-2" type="text" name="last_name_kana" value="<?= $_POST['last_name_kana']; ?>" readonly></span>
+                        <span class="inline-block">セイ：<?= $_POST['first_name_kana']; ?></span>
+                        <span class="inline-block">メイ：<?= $_POST['last_name_kana']; ?></span>
                     </td>
                 </tr>
                 <tr>
                     <th class="contact-item">生年月日<span style="color:red">*</span><br>Date of Birth</th>
                     <td class="contact-body">
-                        <span class="inline-block"><input class="m-2" type="text" name="birth_year" list="birth_year" size="6" value="<?= $_POST['birth_year']; ?>" readonly>年</span>
-                        <span class="inline-block"><input class="m-2" type="text" name="birth_month" list="birth_month" size="3" value="<?= $_POST['birth_month']; ?>" readonly>月</span>
-                        <span class="inline-block"><input class="m-2" type="text" name="birth_day" list="birth_day" size="3" value="<?= $_POST['birth_day']; ?>" readonly>日</span>
+                        <span class="inline-block"><?= $_POST['birth_year']; ?>年</span>
+                        <span class=" inline-block"><?= $_POST['birth_month']; ?>月</span>
+                        <span class=" inline-block"><?= $_POST['birth_day']; ?>日</span>
                     </td>
                 </tr>
                 <tr>
-                    <th class="contact-item">性別<span style="color:red">*</span><br>sex</th>
+                    <th class=" contact-item">性別<span style="color:red">*</span><br>sex</th>
                     <td class="contact-body">
-                        <input class="m-2" type="text" name="sex" value="<?= $_POST['sex']; ?>" readonly>
+                        <?= $_POST['sex']; ?>
                     </td>
                 </tr>
                 <tr>
-                    <th class="contact-item">メールアドレス<span style="color:red">*</span><br>E-mail</th>
+                    <th class=" contact-item">メールアドレス<span style="color:red">*</span><br>E-mail</th>
                     <td class="contact-body">
-                        <span class="inline-block">メール：<input class=" m-2" type="text" name="email" value="<?= $_POST['email']; ?>" readonly></span>
+                        <span class="inline-block">メール：<?= $_POST['email']; ?>< /span>
                     </td>
                 </tr>
                 <tr>
-                    <th class="contact-item">住所<span style="color:red">*</span><br>Address</th>
+                    <th class=" contact-item">住所<span style="color:red">*</span><br>Address</th>
                     <td class="contact-body">
-                        <span class="inline-block">郵便番号<span style="color:red">*</span>：〒<input class="m-2" type="text" name="address_postal" size="12" value="<?= $_POST['address_postal']; ?>" readonly></span>
-                        <span class="inline-block">都道府県<span style="color:red">*</span>：<input class="m-2" type="text" name="address_prefecture" size="12" value="<?= $_POST['address_prefecture']; ?>" readonly></span>
-                        <span class="inline-block">市区町村・町名・丁目<span style="color:red">*</span>：<input class="m-2" type="text" name="address_municipalities" value="<?= $_POST['address_municipalities']; ?>" readonly></span>
-                        <span class="inline-block">番地・号<span style="color:red">*</span>：<input class="m-2" type="text" name="address_number" value="<?= $_POST['address_number']; ?>" readonly></span>
-                        <span class="inline-block">建物名等：<input class="m-2" type="text" name="address_building" value="<?= $_POST['address_building']; ?>"></input></span>
+                        <span class="inline-block">郵便番号<span style="color:red">*</span>：〒<?= $_POST['address_postal']; ?></span>
+                        <span class=" inline-block">都道府県<span style="color:red">*</span>：<?= $_POST['address_prefecture']; ?></span>
+                        <span class=" inline-block">市区町村・町名・丁目<span style="color:red">*</span>：<?= $_POST['address_municipalities']; ?></span>
+                        <span class=" inline-block">番地・号<span style="color:red">*</span>：<?= $_POST['address_number']; ?></span>
+                        <span class=" inline-block">建物名等：<?= $_POST['address_building']; ?></span>
                     </td>
                 </tr>
                 <tr>
                     <th class="contact-item">電話番号<span style="color:red">*</span><br>Phone</th>
-                    <td class="contact-body"><input class="m-2" type="text" name="phone_number" value="<?= $_POST['phone_number']; ?>" readonly></td>
+                    <td class="contact-body"><?= $_POST['phone_number']; ?></td>
                 </tr>
                 <tr>
-                    <th class="contact-item">最終学歴<span style="color:red">*</span><br>Education</th>
-                    <td class="contact-body"><input class="m-2" type="text" name="education" value="<?= $_POST['education']; ?>" readonly>大学</td>
+                    <th class=" contact-item">最終学歴<span style="color:red">*</span><br>Education</th>
+                    <td class="contact-body"><?= $_POST['education']; ?>大学</td>
                 </tr>
                 <tr>
-                    <th class="contact-item">専攻<span style="color:red">*</span><br>Major</th>
+                    <th class=" contact-item">専攻<span style="color:red">*</span><br>Major</th>
                     <td class="contact-body">
-                        <span class="inline-block"><input class="m-2" type="text" name="major" value="<?= $_POST['major']; ?>" readonly></span>
-                        <span class="inline-block">学部：<input class="m-2" type="text" name="major_department" value="<?= $_POST['major_department']; ?>" readonly></span>
-                        <span class="inline-block">学科：<input class="m-2" type="text" name="major_subject" value="<?= $_POST['major_subject']; ?>" readonly></span>
+                        <span class="inline-block"><?= $_POST['major']; ?></span>
+                        <span class=" inline-block">学部：<?= $_POST['major_department']; ?></span>
+                        <span class=" inline-block">学科：<?= $_POST['major_subject']; ?></span>
                     </td>
                 </tr>
                 <tr>
-                    <th class="contact-item">卒業(見込)年月<span style="color:red">*</span><br>Graduation</th>
+                    <th class=" contact-item">卒業(見込)年月<span style="color:red">*</span><br>Graduation</th>
                     <td class="contact-body">
-                        <span class="inline-block"><input class="m-2" type="text" name="graduation_year" size="8" value="<?= $_POST['graduation_year']; ?>" readonly>年</span>
-                        <span class="inline-block"><span class="m-2">3月</span></span>
-                        <span class="inline-block"><input class="m-2" type="text" name="graduation_status" size="10" value="<?= $_POST['graduation_status']; ?>" readonly></span>
+                        <span class="inline-block"><?= $_POST['graduation_year']; ?>年</span>
+                        <span class=" inline-block"><span class="m-2">3月</span></span>
+                        <span class="inline-block"><?= $_POST['graduation_status']; ?></span>
                     </td>
                 </tr>
                 <tr>
-                    <th class="contact-item">自由記入欄<br>Comments</th>
+                    <th class=" contact-item">自由記入欄<br>Comments</th>
                     <td class="contact-body">
-                        <textarea class="" type="text" name="comments" col="50" rows="5" value="<?= $_POST['comments']; ?>" readonly><?= $_POST['comments']; ?></textarea>
+                        <?= $_POST['comments']; ?><?= $_POST['comments']; ?>
                     </td>
                 </tr>
             </table>
-            <div class="relative">
+            <div class=" relative">
                 <button class="absolute top-1 right-1 green" type="button" onclick="history.back()">修正する</button>
             </div>
 
@@ -197,7 +197,6 @@ if ($_SESSION['cart'] == NULL) {
                 $full_name = $_POST['first_name'] . $_POST['last_name'];
                 $full_name_kana = $_POST['first_name_kana'] . $_POST['last_name_kana'];
                 $birth = $_POST['birth_year'] . "-" . $_POST['birth_month'] . "-" . $_POST['birth_day'];
-                echo $birth;
                 $address = $_POST['address_postal'] . $_POST['address_prefecture'] . $_POST['address_municipalities'] . $_POST['address_number'] . $_POST['address_building'];
                 ?>
                 <form action="./complete.php" method="post">
