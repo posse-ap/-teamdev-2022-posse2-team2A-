8eceeac5182eda90e87b1b0c585ec6c843f319bd
Eiki Watanabe <basketball.slamdunk1@icloud.com>
2022-05-28 04:54:23 +0900

    path設定

--- src/agent/login.php
+++ src/agent/login.php
@@ -55,7 +55,7 @@ if (!empty($_POST)) {
             <a href="" class="text-xs">パスワードを忘れた方へ</a>
         </div>
         <div class="text-center m-10">
-            <a class="newAgent_input_button" href="../user/new_apply/newAgentform.php">お申し込み</a>
+            <a class="newAgent_input_button" href="./new_apply/newAgentform.php">お申し込み</a>
             <p class="text-xs m-5">新たに自社情報の掲載を希望される方は"お申し込みボタン"から登録をお願いいたします。</p>
         </div>
     </main>
