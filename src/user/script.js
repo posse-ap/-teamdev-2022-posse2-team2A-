1e45c241671f467a7232daa27e46877b9ebe0f35
takahara tomoaki <jihyun8286@gmail.com>
2022-05-29 22:03:03 +0900

    mainFooter関連のcssとjsを削除

--- src/user/script.js
+++ src/user/script.js
@@ -9,30 +9,6 @@ $("#g-nav a").click(function () {//ナビゲーションのリンクがクリッ
 });
 // headerのハンバーガーメニューの動きここまで
 
-// 下にスクロール時footerのアイコンが消え、上にスクロールすると現れる動きここから
-
-var beforePos = 0;//スクロールの値の比較用の設定
-
-
-//スクロール途中でヘッダーが消え、上にスクロールすると復活する設定を関数にまとめる
-function ScrollAnime() {
-    var elemTop = $('#footer').offset().top;//#footerの位置まできたら
-    var scroll = $(window).scrollTop();
-    //ヘッダーの出し入れをする
-    if(scroll == beforePos) {
-		//IE11対策で処理を入れない
-    }else if(elemTop < scroll || 0 < scroll - beforePos){
-		//ヘッダーが上から出現する
-		$('#mainFooter').removeClass('UpMove');	//#mainFooterにUpMoveというクラス名を除き
-		$('#mainFooter').addClass('DownMove');//#mainFooterにDownMoveのクラス名を追加
-    }else {
-		//ヘッダーが上に消える
-        $('#mainFooter').removeClass('DownMove');//#mainFooterにDownMoveというクラス名を除き
-        $('#mainFooter').addClass('UpMove');//#mainFooterにUpMoveのクラス名を追加
-    }
-    
-    beforePos = scroll;//現在のスクロール値を比較用のbeforePosに格納
-}
 
 // タブの動きここから
 document.addEventListener('DOMContentLoaded', function(){
@@ -108,13 +84,6 @@ function next(){
 
 // 月ごとの報酬を表示するここまで
 
-
-
-
-// 画面をスクロールをしたら動かしたい場合の記述
-$(window).scroll(function () {
-	ScrollAnime();//スクロール途中でヘッダーが消え、上にスクロールすると復活する関数を呼ぶ
-});
 
 
 // 下にスクロール時footerのアイコンが消え、上にスクロールすると現れる動きここまで
